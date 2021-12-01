782d894dd7d82e8c808fa3b55b0bdaab121a9fad
robloxboy100058 <83027818+robloxboy100058@users.noreply.github.com>
2021-12-01 17:44:40 -0600

    Release v1.00

---
+++ IntNetViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IntNetViewer
+//
+//  Created by Christian Chapa on 11/22/21.
+//  Copyright © 2021 robloxboy1000. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IntNetViewer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IntNetViewer
+//
+//  Created by Christian Chapa on 11/22/21.
+//  Copyright © 2021 robloxboy1000. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
